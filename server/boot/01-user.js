f8bdf79b095e3ae3a852bffe9181b99b1f23e49c
Daniel Ulises Garcia Verdin <dan@hectic.io>
2016-07-20 19:46:38 -0500

    Adding some users and retriving wildpokemons with users

---
+++ server/boot/01-user.js
@@ -0,0 +1,56 @@
+'use strict';
+
+// to enable these logs set `DEBUG=boot:02-load-users` or `DEBUG=boot:*`
+var log = require('debug')('boot:02-load-users');
+
+module.exports = function(app) {
+
+  createDefaultUsers();
+
+  function createDefaultUsers() {
+
+    log('Creating roles and users');
+
+    var User = app.models.Trainer;
+
+    var users = [];
+    var roles = [{
+      name: 'admin',
+      users: [{
+        firstName: 'Admin',
+        lastName: 'User',
+        email: 'admin@admin.com',
+        username: 'poketests42',
+        password: 'piripe'
+      }]
+    }, {
+      name: 'users',
+      users: [{
+        firstName: 'Guest',
+        lastName: 'User',
+        email: 'user@user.com',
+        username: 'user',
+        password: 'user'
+      }]
+    }];
+
+    roles.forEach(function(role) {
+      role.users.forEach(function(roleUser) {
+        User.findOrCreate({
+            where: {
+              username: roleUser.username
+            }
+          }, // find
+          roleUser, // create
+          function(err, createdUser, created) {
+            if (err) {
+              console.error('error creating roleUser', err);
+            }
+            (created) ? log('created user', createdUser.username): log('found user', createdUser.username);
+          });
+      });
+    });
+    return users;
+  }
+
+};
--- server/server.js
+++ server/server.js
@@ -3,27 +3,6 @@ var boot = require('loopback-boot');
 
 var app = module.exports = loopback();
 
-app.use(loopback.context());
-app.use(loopback.token());
-app.use(function setCurrentUser(req, res, next) {
-  if (!req.accessToken) {
-    return next();
-  }
-  app.models.UserModel.findById(req.accessToken.userId, function(err, user) {
-    if (err) {
-      return next(err);
-    }
-    if (!user) {
-      return next(new Error('No user with this access token was found.'));
-    }
-    var loopbackContext = loopback.getCurrentContext();
-    if (loopbackContext) {
-      loopbackContext.set('currentUser', user);
-    }
-    next();
-  });
-});
-
 app.start = function() {
   // start the web server
   return app.listen(function() {
