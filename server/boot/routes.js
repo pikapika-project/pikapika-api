b0d58f12a9daa7a47da0261cffcfb1f7165d1651
Tuxity <kevin.darcel@gmail.com>
2016-07-22 12:01:16 -0500

    Separate login and heartbeat functions in 2 routes

--- server/boot/routes.js
+++ server/boot/routes.js
@@ -1,65 +1,87 @@
-var Pokeio = require('pokemon-go-node-api');
+var PokemonGO = require('pokemon-go-node-api');
+
+var trainers = [];
 
 module.exports = function(app) {
 
-  app.post('/login', function(req, res) {
+  app.post('/trainer/login', function(req, res) {
 
-    if (req.body) {
-      var trainer = {
-        username: req.body.username,
-        password: req.body.password,
-        location: {
-          type: 'coords',
-          name: req.body.location.name,
-          coords: {
-            latitude: req.body.location.coords.latitude,
-            longitude: req.body.location.coords.longitude,
-            altitude: req.body.location.coords.altitude
-          }
-        },
-        provider: req.body.provider
-      };
+    if (!req.body) {
+      res.status(404).json({error: {statusCode: 404, statusMessage: "Missing parameters."}});
+    }
 
-      var PokemonGO = require('pokemon-go-node-api');
-      var Pokeio = new PokemonGO.Pokeio();
-      var WildPokemons = [];
-
-      function sendError(trainer, err, res) {
-        var statusCode, statusMessage;
-
-        if (err instanceof Error) {
-          statusCode = err.statusCode || 400;
-          statusMessage = err.message;
-        } else {
-          statusCode = err.response.statusCode || 400;
-          statusMessage = err.response.statusMessage;
+    var trainer = {
+      username: req.body.username,
+      password: req.body.password,
+      location: {
+        type: 'coords',
+        name: req.body.location.name,
+        coords: {
+          latitude: req.body.location.coords.latitude,
+          longitude: req.body.location.coords.longitude,
+          altitude: req.body.location.coords.altitude
         }
+      },
+      provider: req.body.provider
+    };
 
-        res.status(statusCode).json({error: {statusCode: statusCode, statusMessage: statusMessage}});
+    var Pokeio = new PokemonGO.Pokeio();
+
+    function sendError(trainer, err, res) {
+      var statusCode, statusMessage;
+
+      if (err instanceof Error) {
+        statusCode = err.statusCode || 400;
+        statusMessage = err.message;
+      } else {
+        statusCode = err.response.statusCode || 400;
+        statusMessage = err.response.statusMessage;
       }
 
-      Pokeio.init(trainer.username, trainer.password, trainer.location, trainer.provider, function(err) {
-        if (err) {
-          sendError(trainer, err, res);
-          return false;
-        }
-        Pokeio.Heartbeat(function(err, hb) {
-          if (err) {
-            sendError(trainer, err, res);
-            return false;
-          }
-
-          hb.cells.forEach(function(cell) {
-            if (cell.WildPokemon.length > 0) {
-              WildPokemons = cell.WildPokemon;
-              WildPokemons.forEach(function(wildPokemon, i) {
-                wildPokemon.pokeinfo = Pokeio.pokemonlist[wildPokemon.pokemon.PokemonId - 1];
-              });
-            }
-          });
-          res.json({data: WildPokemons});
-        });
-      });
+      res.status(statusCode).json({error: {statusCode: statusCode, statusMessage: statusMessage}});
     }
+
+    Pokeio.init(trainer.username, trainer.password, trainer.location, trainer.provider, function(err) {
+      if (err) {
+        sendError(trainer, err, res);
+        return false;
+      }
+
+      trainers[Pokeio.playerInfo.accessToken] = Pokeio.playerInfo;
+      var data = {apiEndpoint: Pokeio.playerInfo.apiEndpoint, accessToken: Pokeio.playerInfo.accessToken};
+
+      res.json({data: data});
+    });
   });
+
+  app.get('/pokemons/heartbeat', function(req, res) {
+
+    if (!req.query.accessToken) {
+      res.status(404).json({error: {statusCode: 404, statusMessage: "Missing parameters."}});
+    }
+
+    var Pokeio = new PokemonGO.Pokeio();
+    Pokeio.playerInfo = trainers[req.query.accessToken];
+
+    var WildPokemons = [];
+
+    Pokeio.Heartbeat(function(err, hb) {
+      if (err) {
+        sendError(trainer, err, res);
+        return false;
+      }
+
+      hb.cells.forEach(function(cell) {
+        if (cell.WildPokemon.length > 0) {
+          WildPokemons = cell.WildPokemon;
+          WildPokemons.forEach(function(wildPokemon, i) {
+            wildPokemon.pokeinfo = Pokeio.pokemonlist[wildPokemon.pokemon.PokemonId - 1];
+          });
+        }
+      });
+
+      res.json({data: WildPokemons});
+    });
+  });
+
 }
