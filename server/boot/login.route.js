31706060f92a49628425a1dedefeaccb8b5f8046
Sebastián Osorio <im@svzosorio.co>
2016-07-29 16:07:55 -0500

    Solver login

--- server/boot/login.route.js
+++ server/boot/login.route.js
@@ -1,40 +1,41 @@
 var PokemonGO = require('pokemon-go-node-api');
-var Promise = require("bluebird");
 
 module.exports = function(app) {
 
   app.post('/trainers/login', function(req, res) {
 
+    console.log("[i] Login from " + req.headers['cf-ipcountry'] + " ("+ req.headers['x-forwarded-for'] + ")");
+
     if (!req.body) {
       res.status(404).json({
         error: {
-          statusCode: 404,
+          statusCode   : 404,
           statusMessage: "Missing parameters."
         }
       });
     }
 
-    var Pokeio        = new PokemonGO.Pokeio();
-    var PokemonGoInit = Promise.promisify(Pokeio.init);
-    var now;
-
     var trainer = req.body;
-    var filter  = {
-      where: {
-        username: trainer.username,
-        provider: trainer.provider.name
+    var Pokeio  = new PokemonGO.Pokeio();
+
+    Pokeio.init(trainer.username, trainer.location, trainer.provider, function(err, session) {
+      if (err) {
+        sendError(err, res);
+        return false;
       }
-    };
 
-    Trainer = app.models.trainer;
-
-    PokemonGoInit().then(session => {
-
+      Trainer = app.models.trainer;
+      var filter = {
+        where: {
+          username: trainer.username,
+          provider: trainer.provider.name
+        }
+      };
       var newTrainer = {
-        username:    trainer.username,
-        accessToken: trainer.provider.token,
-        provider:    trainer.provider.name,
-        apiEndpoint: session.apiEndpoint
+        username   : trainer.username,
+        accessToken: Pokeio.playerInfo.accessToken,
+        provider   : trainer.provider.name,
+        apiEndpoint: Pokeio.playerInfo.apiEndpoint
       }
 
       Trainer.findOrCreate(filter, newTrainer, function(err, createdTrainer, created) {
@@ -42,51 +43,41 @@ module.exports = function(app) {
           sendError(err, res);
         }
 
-        now = new Date();
-
-        if (created) {
-          newTrainer.createdAt = now;
-          newTrainer.updatedAt = now;
-        } else {
-          newTrainer.updatedAt = now;
+        if (!created) {
+          createdTrainer.updateAttributes(newTrainer, function(err, instance) {
+            if (err) {
+              sendError(err, res);
+            }
+          });
         }
-
-        createdTrainer.updateAttributes(newTrainer, function(err, instance) {
-          if (err) {
-            sendError(err, res);
-          }
-        });
       });
 
       res.json({
         data: {
-          access_token: trainer.provider.accessToken,
-          expire_time:  session.tokenExpireTime
+          access_token: session.accessToken,
+          expire_time : session.expireTime
         }
       });
-
-    }).catch(err => {
-      if (err) {
-        sendError(err, res);
-      }
     });
-
   });
 
   function sendError(err, res) {
+
+    console.log(err);
+
     var statusCode, statusMessage;
 
     if (err instanceof Error) {
-      statusCode    = err.statusCode || 400;
+      statusCode = err.statusCode || 400;
       statusMessage = err.message;
     } else {
-      statusCode    = err.response.statusCode || 400;
+      statusCode = err.response.statusCode || 400;
       statusMessage = err.response.statusMessage;
     }
 
     res.status(statusCode).json({
       error: {
-        statusCode:    statusCode,
+        statusCode: statusCode,
         statusMessage: statusMessage
       }
     });
