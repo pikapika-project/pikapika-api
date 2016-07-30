34957502e793d2a74afcbad6cea8bcd22cee991d
Tuxity <kevin.darcel@gmail.com>
2016-07-29 20:02:44 -0500

    Some return pleasplease

--- server/boot/heartbeat.route.js
+++ server/boot/heartbeat.route.js
@@ -18,6 +18,7 @@ module.exports = function(app) {
           statusMessage: "Missing parameters."
         }
       });
+      return;
     }
 
     let qs = [];
--- server/boot/login.route.js
+++ server/boot/login.route.js
@@ -13,6 +13,7 @@ module.exports = function(app) {
           statusMessage : "Missing parameters."
         }
       });
+      return;
     }
 
     var filter = {
