45981e9b9542bf75f655b6050d6d7ae6b8dad560
Tuxity <kevin.darcel@gmail.com>
2016-08-01 19:48:41 -0500

    Rename file, cleanup

--- server/boot/pokemon.v3.routes.js
+++ server/boot/pokemon.v3.routes.js
@@ -41,7 +41,7 @@ module.exports = function(app) {
       return client.init();
     })
     .then(() => {
-      var cellIDs = pogobuf.Utils.getCellIDs(lat, lng, 8);
+      var cellIDs = pogobuf.Utils.getCellIDs(lat, lng, 5);
 
       return bluebird
         .resolve(client.getMapObjects(cellIDs, Array(cellIDs.length).fill(0)))
@@ -52,6 +52,7 @@ module.exports = function(app) {
           return bluebird.resolve(cell.wild_pokemons)
             .each(pokemon => {
               now = new Date();
+
               if (!isExist(pokemons, pokemon)) {
                 pokemons.push({
                   id:       pokemon.spawn_point_id,
@@ -117,69 +118,6 @@ module.exports = function(app) {
   /*
    * UTILS functions
    */
-  function get_cell_ids(lat, lng) {
-    let origin = new s2.S2CellId(new s2.S2LatLng(lat, lng)).parent(15);
-    let walk = [origin.id()];
-    // 10 before and 10 after
-    let next = origin.next();
-    let prev = origin.prev();
-    for (let i = 0; i < 10; i++) {
-      // in range(10):
-      walk.push(prev.id());
-      walk.push(next.id());
-      next = next.next();
-      prev = prev.prev();
-    }
-    return walk.sort();
-  }
-
-  function generateSpiral(startingLat, startingLng, stepSize, stepLimit) {
-    let coords = [{
-      'lat': startingLat,
-      'lng': startingLng
-    }];
-
-    let steps = 1;
-    let x = 0;
-    let y = 0;
-    let d = 1;
-    let m = 1;
-    let rlow = 0.0;
-    let rhigh = 0.0005;
-
-    while (steps < stepLimit) {
-      while (2 * x * d < m && steps < stepLimit) {
-        x = x + d;
-        steps += 1;
-        let random = (Math.random() * (rlow - rhigh) + rhigh).toFixed(4);
-        let random2 = (Math.random() * (rlow - rhigh) + rhigh).toFixed(4);
-        let lat = x * stepSize + startingLat + random;
-        let lng = y * stepSize + startingLng + random2;
-
-        coords.push({
-          'lat': lat,
-          'lng': lng
-        });
-      }
-
-      while (2 * y * d < m && steps < stepLimit) {
-        y = y + d;
-        steps += 1;
-        let lat = x * stepSize + startingLat + (Math.random() * (rlow - rhigh) + rlow);
-        let lng = y * stepSize + startingLng + (Math.random() * (rlow - rhigh) + rlow);
-
-        coords.push({
-          'lat': lat,
-          'lng': lng
-        });
-      }
-      d = -1 * d;
-      m = m + 1;
-    }
-
-    return coords;
-  }
-
   function isExist(pokemons, pokemon) {
     return pokemons.some(function (p) {
       return p.id === pokemon.spawn_point_id;
