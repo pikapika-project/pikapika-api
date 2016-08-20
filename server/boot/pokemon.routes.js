e0849262b18f53c35c213b4d18c6e5913c1c7f14
Tuxity <kevin.darcel@gmail.com>
2016-08-20 12:18:08 -0500

    typoe

--- server/boot/pokemon.routes.js
+++ server/boot/pokemon.routes.js
@@ -112,7 +112,7 @@ module.exports = function(app) {
           });
 
           if (pokemons.length) {
-            console.log(pokemons.length, "pokemon scanned");
+            console.log("\t", pokemons.length, "pokemon scanned");
             let where = {
               _id: {
                 inq: pokemons.map(function(p) { return p.id; })
