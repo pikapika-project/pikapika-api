32fa72c364beff82e2e60c89e13dc872bc0595fa
Tuxity <kevin.darcel@gmail.com>
2016-08-23 23:43:33 -0500

    Add apk_url, update version

--- server/boot/configuration.routes.js
+++ server/boot/configuration.routes.js
@@ -7,11 +7,12 @@ module.exports = function(app) {
     var config = {
       last_version: {
         ios: "1.0.0",
-        android: "1.0.0"
+        android: "1.0.2"
       },
       ads: {
         reload_number: 5
-      }
+      },
+      apk_url: "https://pikapika.io/pikapika-v120.apk"
     };
 
     res.json({
